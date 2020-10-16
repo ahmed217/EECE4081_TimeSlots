3254e3d7d15749dcbee9c1196bbfa93e7a0f024e
Brandon Van Pelt <brandonvanpelt@outlook.com>
2020-10-16 14:53:26 -0500

    new format

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ database = (
         )
     """
 
-database = "sqlite:///timeslots.db"
-
+database = "sqlite:///timeSlots.db"
+#{{ timeSlots.daysOfWeek }} {{ timeSlots.startTime }} {{ timeSlots.startTimeAMPM }} - {{ timeSlots.endTime }} {{ timeSlots.endTimeAMPM  }}
 app = Flask(__name__)
 app.secret_key = 'test'
 # important configuration parameter, don't miss it 
@@ -36,67 +36,93 @@ db = SQLAlchemy(app)
 @app.route('/') 
 @app.route('/index')
 def index():
-    timeslot = timeslots.query.all()
+    timeslot = timeSlots.query.all()
     return render_template("index.html",timeslot=timeslot, title = 'Home')
 
     
 @app.route('/create', methods=['GET','POST'])
 def create():
+    daysOfWeek = ''
+    isFirst = True
     if request.form:
         if request.form.get("Sunday") == 'true':
-            Sunday = True
-        else:
-            Sunday = False
+            if isFirst == True:
+                daysOfWeek +=  'U'
+                isFirst == False
+            else:
+                daysOfWeek += ',' + 'U'
 
+    if request.form:
         if request.form.get("Monday") == 'true':
-            Monday = True
-        else:
-            Monday = False
+            if isFirst == True:
+                daysOfWeek +=  'M'
+                isFirst == False
+            else:
+                daysOfWeek += ',' + 'M'
 
+    if request.form:
         if request.form.get("Tuesday") == 'true':
-            Tuesday = True
-        else:
-            Tuesday = False
+            if isFirst == True:
+                daysOfWeek +=  'T'
+                isFirst == False
+            else:
+                daysOfWeek += ',' + 'T'
 
+    if request.form:
         if request.form.get("Wednesday") == 'true':
-            Wednesday = True
-        else:
-            Wednesday = False
-            
+            if isFirst == True:
+                daysOfWeek +=  'W'
+                isFirst == False
+            else:
+                daysOfWeek += ',' + 'W'
+
+    if request.form:
         if request.form.get("Thursday") == 'true':
-            Thursday = True
-        else:
-            Thursday = False
-            
+            if isFirst == True:
+                daysOfWeek +=  'R'
+                isFirst == False
+            else:
+                daysOfWeek += ',' + 'R'
+
+    if request.form:
         if request.form.get("Friday") == 'true':
-            Friday = True
-        else:
-            Friday = False
-            
+            if isFirst == True:
+                daysOfWeek +=  'F'
+                isFirst == False
+            else:
+                daysOfWeek += ',' + 'F'
+
+    if request.form:
         if request.form.get("Saturday") == 'true':
-            Saturday = True
-        else:
-            Saturday = False
+            if isFirst == True:
+                daysOfWeek +=  'S'
+                isFirst == False
+            else:
+                daysOfWeek += ',' + 'S'
+
+        
             
         startTime = request.form.get("StartTime")
+        startTime_AMPM = request.form.get("startTime_AMPM")
         endTime = request.form.get("EndTime")
+        endTime_AMPM = request.form.get("endTime_AMPM")
         
-        newslot = timeslots(Sunday = Sunday, Monday = Monday, Tuesday = Tuesday, Wednesday = Wednesday, Thursday = Thursday, Friday = Friday, Saturday = Saturday, startTime = startTime, endTime = endTime)
+        newslot = timeSlots(daysOfWeek = daysOfWeek, startTime = startTime, startTime_AMPM = startTime_AMPM, endTime = endTime, endTime_AMPM = endTime_AMPM)
         db.session.add(newslot)
         db.session.commit()
         
         
-    newslot = timeslots.query.all()
+    newslot = timeSlots.query.all()
     return render_template("create.html",newslot = newslot, title = 'Create')   
 
 
 
 @app.route('/delete/<timeslot_id>') 
 def delete(timeslot_id):
-        timeslot = timeslots.query.get(timeslot_id)
+        timeslot = timeSlots.query.get(timeslot_id)
         db.session.delete(timeslot)
         db.session.commit()
-        timeslot = timeslots.query.all()
+        timeslot = timeSlots.query.all()
         return render_template("index.html",timeslot=timeslot,title ='Home')
 
 
@@ -104,31 +130,30 @@ def delete(timeslot_id):
 @app.route('/update', methods = ['GET', 'POST'])
 def update():
         if request.method == 'POST':
-            my_data = timeslots.query.get(request.form.get('id'))
+            my_data = timeSlots.query.get(request.form.get('id'))
             my_data.startTime = request.form['startTime']
+            my_data.startTime_AMPM = request.form['startTime_AMPM']
             my_data.endTime = request.form['endTime']
+            my_data.endTime_AMPM = request.form['endTime_AMPM']
+            my_data.daysOfWeek = request.form['daysOfWeek']
             db.session.commit()
-            timeslot = timeslots.query.all()
-            return render_template("index.html", timeslot=timeslot, title = 'Create') 
+            timeslot = timeSlots.query.all()
+            return render_template("index.html", timeslot=timeslot, title = 'Home') 
 
-        return render_template("index.html", timeslot=timeslot, title = 'Create') 
+        return render_template("index.html", timeslot=timeslot, title = 'Home') 
 
 
 
 # this class creates a table in the database named TimeSlot with 
 # entity fields id as integer, brand as text, and price as decimal number 
 # create a module containing this class and import that class into this application and use it
-class timeslots(db.Model):
+class timeSlots(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    Sunday = db.Column(db.Boolean, default = False)
-    Monday = db.Column(db.Boolean, default = False)
-    Tuesday = db.Column(db.Boolean, default = False)
-    Wednesday = db.Column(db.Boolean, default = False)
-    Thursday = db.Column(db.Boolean, default = False)
-    Friday = db.Column(db.Boolean, default = False)
-    Saturday = db.Column(db.Boolean, default = False)
+    daysOfWeek = db.Column(db.String(255), nullable = False)
     startTime = db.Column(db.String(255), nullable = False)
+    startTime_AMPM = db.Column(db.String(255), nullable = False)
     endTime = db.Column(db.String(255), nullable = False)
+    endTime_AMPM = db.Column(db.String(255), nullable = False)
 
 #if __name__ == '__main__':
 #    app.run(debug=True)
